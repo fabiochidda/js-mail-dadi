6537a1547b10ced59e488ef00aebba350be1a1f1
fabiochidda <fabio.chidda99@gmail.com>
2022-02-09 20:26:39 +0100

    Aggiunto alert e l'array con le email, dichiarata la variabile con il prompt

---
+++ app.js
@@ -0,0 +1,10 @@
+//Alert di benvenuto
+alert("Benvenuto nel gioco dei dadi");
+
+//Dichiarata variabile con il prompt per far inserire la email all'utente
+let userEmail = prompt("Inserisci la tua email per iniziare a giocare");
+console.log(userEmail);
+
+// Dichiarato l'array che contiene le email
+const diceGameEmail = ["paperino@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "minnie@gmail.com"];
+console.log(diceGameEmail)
