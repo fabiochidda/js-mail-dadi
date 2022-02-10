e650346d5fe8934306172f250ef6212f312816fa
fabiochidda <fabio.chidda99@gmail.com>
2022-02-10 02:54:42 +0100

    Aggiunto il ciclo per controllare i dati inseriti dall'utente

--- app.js
+++ app.js
@@ -7,4 +7,33 @@ console.log(userEmail);
 
 // Dichiarato l'array che contiene le email
 const diceGameEmail = ["paperino@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "minnie@gmail.com"];
-console.log(diceGameEmail)
+console.log(diceGameEmail);
+
+// Ciclo per controllare se i dati inseriti dall'utente corrispondono a quelli nell'array
+
+let diceGameEmailFound = 0;
+
+for (let i=0; i < diceGameEmail.length; i++ ) {
+
+    console.log(i);
+
+    const diceGameEmailList = diceGameEmail[i];
+
+    if (userEmail == diceGameEmailList) {
+
+        diceGameEmailFound = 1;
+
+    } 
+
+}
+
+if (diceGameEmailFound == 1) {
+
+    alert("Hai effettuato l'accesso");
+
+} else {
+
+    alert("Per favore inserisci una email registrata");
+
+}
+
